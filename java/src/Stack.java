d61ebc98c5df833e729c476bb7741dd432680440
aylenlee1234 <aylen.lee.1972@gmail.com>
2025-04-14 20:55:34 -0300

    added preconditions and postconditions to Stack Interface

--- java/src/Stack.java
+++ java/src/Stack.java
@@ -9,8 +9,22 @@ public interface Stack {
      * el calor valor dado en el tope.
      * @param a es un valor a apilar
      */
+
+    /**
+     * - Postcondición: cada vez que se agrega un elemento nuevo, este se agrega al tope y se crea una nueva versión
+     */
     void add(int a);
+
+    /**
+     * - Precondición: la pila no está vacía
+     * - Postcondición: se elimina un elemento del tope y se genera una nueva versión
+     */
     void remove();
+
+    /**
+     * - Precondición: la pila no está vacía
+     * - Postcondición: se obtiene un elemento del tope
+     */
     int getTop();
     boolean isEmpty();
 
